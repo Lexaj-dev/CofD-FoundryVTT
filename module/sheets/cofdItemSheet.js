edca6d4e72dc64a99e8ae63d28b332739853eeac
Lexaj-dev <kristof.reimers@gmail.com>
2020-12-02 13:22:26 +0100

    Added basic item sheeet

--- module/sheets/cofdItemSheet.js
+++ module/sheets/cofdItemSheet.js
@@ -1,5 +1,14 @@
 export default class cofdItemSheet extends ItemSheet {
-    get template(){
+    
+    get template() {
         return `systems/cofd/templates/sheets/${this.item.data.type}-sheet.html`;
+    }
+
+    getData() {
+        const data = super.getData();
+
+        data.config = CONFIG.cofd;
+
+        return data;
     }
 }
