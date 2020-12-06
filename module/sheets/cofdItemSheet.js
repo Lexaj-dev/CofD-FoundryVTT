1763f04ad50be08d3d15ac7f7b5c67e9a28a6236
Lexaj-dev <kristof.reimers@gmail.com>
2020-12-06 17:02:33 +0100

    Added CSS via Less

--- module/sheets/cofdItemSheet.js
+++ module/sheets/cofdItemSheet.js
@@ -1,5 +1,13 @@
 export default class cofdItemSheet extends ItemSheet {
     
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 530, 
+            height: 340,
+            classes: ["cofd", "sheet", "item"] 
+        })
+    }
+
     get template() {
         return `systems/cofd/templates/sheets/${this.item.data.type}-sheet.html`;
     }
